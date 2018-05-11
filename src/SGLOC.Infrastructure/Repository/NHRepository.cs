5434dabb0e39d17179106fab0ac79b32e4a582d7
MαykєL Bяυиσ <maykelbrn@hotmail.com>
2018-05-11 15:50:06 -0400

    Adiciona Repository em Infraestructure

---
+++ src/SGLOC.Infrastructure/Repository/NHRepository.cs
@@ -0,0 +1,41 @@
+﻿using SGLOC.ApplicationCore.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SGLOC.Infrastructure.Repository
+{
+    class NHRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        public TEntity Adicionar(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> Buscar(Expression<Func<TEntity, bool>> predicado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remover(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
